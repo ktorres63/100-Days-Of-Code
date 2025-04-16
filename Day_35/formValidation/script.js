0451753313cdbf00defb1ae6b9090d7464a6741a
ktorres63 <ktorresaro@unsa.edu.pe>
2025-04-15 23:16:57 -0500

    Finish exercise

--- Day_35/formValidation/script.js
+++ Day_35/formValidation/script.js
@@ -9,24 +9,65 @@ const countryError = document.getElementById("countryError");
 const postalError = document.getElementById("postalError");
 const passwordError = document.getElementById("passwordError");
 const confirmError = document.getElementById("confirmError");
+let hasInteracted = false;
 
-function validateEmail(){
-  const valid = email.value.includes("@");
-  toggleValidation(email, emailError,valid)
-  return valid
+function validateEmail() {
+  const valid = email.checkValidity();
+  toggleValidation(emailError, valid);
+  return valid;
 }
-function toggleValidation(input, errorElement, isValid){
-  if(isValid){
-    input.classList.remove("invalid");
-    input.classList.add("valid");
+function validateCountry() {
+  const valid = country.checkValidity();
+  toggleValidation(countryError, valid);
+  return valid;
+}
+function validatePostal() {
+  const valid = !postal.validity.patternMismatch;
+  toggleValidation(postalError, valid);
+  return valid;
+}
+function validatePassword() {
+  const valid = password.checkValidity();
+  toggleValidation(passwordError, valid);
+  return valid;
+}
+function validateConfirmPassword() {
+  const valid =
+    password.value === confirmPassword.value && confirmPassword !== "";
+  toggleValidation(confirmError, valid);
+  return valid;
+}
+
+function toggleValidation(errorElement, isValid) {
+  if (isValid) {
     errorElement.classList.remove("active");
-  }
-  else{
-    input.classList.add("invalid");
-    input.classList.remove("valid");
-    errorElement.classList.add("active")
+  } else {
+    errorElement.classList.add("active");
   }
 }
-
 
 email.addEventListener("input", validateEmail);
+country.addEventListener("input", validateCountry);
+postal.addEventListener("input", validatePostal);
+password.addEventListener("input", () => {
+  validatePassword();
+  validateConfirmPassword();
+});
+confirmPassword.addEventListener("input", validateConfirmPassword);
+
+document.getElementById("myForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const isFormValid =
+    validateEmail() &&
+    validateCountry() &&
+    validatePostal() &&
+    validatePassword() &&
+    validateConfirmPassword();
+
+  if (isFormValid) {
+    alert("form submitted sucessfully")
+  }
+  else{
+    alert("Fix errors")
+  }
+});
